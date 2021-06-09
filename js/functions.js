b5b737e3ec0f74a3cd2651233784236fa1a66fdf
Agata Szulejko <amoranska@poczta.wp.pl>
2021-06-09 17:51:42 +0200

    draw-next-step-eight

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,44 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(argMoveId){
+  if (argMoveId == 1) {
+		return 'kamień';
+	}
+	else if (argMoveId == 2) {
+		return 'nożyce';
+	}
+	else if (argMoveId == 3) {
+		return 'papier';
+	}
+	else {
+		printMessages('Nie znam ruchu o id ' + argMoveId + '.');
+		return 'nieznany ruch';
+	}
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+
+	printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+   (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
+   (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
+    printMessage('Ty wygrywasz!');
+  }
+
+if ((argComputerMove == 'papier' && argPlayerMove == 'kamień') ||
+    (argComputerMove == 'nożyce' && argPlayerMove == 'papier') ||
+    (argComputerMove == 'kamień' && argPlayerMove == 'nożyce')) {
+    printMessage('Komputer wygrywa!');
+  }
+
+if (argComputerMove == argPlayerMove) || {
+    printMessage('Mamy remis!');
+  }
+
+ if (argPlayerMove == 'nieznany ruch') {
+    printMessage('Powtórz ruch gracza');
+}
+}
