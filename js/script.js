5d40851024109eca6cfa46ab020d1a32a51fb7b7
Agata Szulejko <amoranska@poczta.wp.pl>
2021-06-09 21:42:12 +0200

    draw-next-step-ten

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function playGame(playerInput){
+clearMessages();
+let randomNumber = Math.floor;(Math.random() * 3 + 1);
 let computerMove = getMoveName(randomNumber);
 
 printMessage('Mój ruch to: ' + computerMove);
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: nożyce, 3: papier.');
 
 let playerMove = getMoveName(playerInput);
 
 displayResult(computerMove, playerMove);
+}
+
+let playScissors = document.getElementById('play-scissors');
+playScissors.addEventListener('click', function(){
+  playGame(2);
+});
+
+let playRock = document.getElementById('play-rock');
+playRock.addEventListener('click', function(){
+  playGame(1);
+});
+
+let playPaper = document.getElementById('play-paper');
+playPaper.addEventListener('click',function(){
+  playGame(3);
+});
