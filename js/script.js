e5a04826e1f87a6418a16590fa2422f2203d1898
Agata Szulejko <amoranska@poczta.wp.pl>
2021-06-09 14:58:25 +0200

    draw-next-step-four

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
+('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
 
 if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumber == 2) {
     computerMove = 'nożyce';
-} else {
+} else if  (randomNumber == 3) {
     computerMove = 'papier';
 }
 printMessage('Mój ruch to: ' + computerMove);
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: nożyce, 3: papier.');
-console.log('Gracz wpisał: ' + playerInput);
+('Gracz wpisał: ' + playerInput);
+
 let playerMove = 'nieznany ruch';
+
 if (playerInput == '1') {
     playerMove = 'kamień';
 } else if (playerInput == '2') {
     playerMove = 'nożyce';
-} else {
+} else if (playerInput == '3'){
     playerMove = 'papier';
 }
+
 printMessage('Twój ruch to: ' + playerMove);
 
 if ((computerMove == 'kamień' && playerMove == 'papier') ||
@@ -40,6 +43,6 @@ if ((computerMove == 'papier' && playerMove == 'papier') ||
     printMessage('Mamy remis!');
   }
 
- if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+ if (playerMove == 'nieznany ruch') {
     printMessage('Powtórz ruch gracza');
 }
